0eeb3756bdf9302d0ecb5f3c086517460482b837
Tomas Skalicky <skalicky.tomas@gmail.com>
2020-05-25 20:56:22 +0200

    reverse_linked_list.py added

---
+++ src/main/python/com/skalicky/python/smallpuzzles/reverse_linked_list.py
@@ -0,0 +1,80 @@
+# Task:
+#
+# Given a singly-linked list, reverse the list. This can be done iteratively or recursively. Can you get both solutions?
+#
+# Example:
+# Input: 4 -> 3 -> 2 -> 1 -> 0 -> NULL
+# Output: 0 -> 1 -> 2 -> 3 -> 4 -> NULL
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next: ListNode = None
+
+    # Function to print the list
+    def print_list(self):
+        node = self
+        output = ''
+        while node is not None:
+            output += str(node.val)
+            output += " "
+            node = node.next
+        print(output)
+
+    # Iterative Solution
+    def reverse_iteratively(self):
+        previous_node: ListNode = None
+        current_node: ListNode = self
+        next_node: ListNode = current_node.next
+        while current_node is not None:
+            current_node.next = previous_node
+            previous_node: ListNode = current_node
+            current_node: ListNode = next_node
+            if current_node is not None:
+                next_node: ListNode = current_node.next
+            else:
+                next_node: ListNode = None
+
+    # Recursive Solution
+    def reverse_recursively(self, previous_node=None):
+        next_node: ListNode = self.next
+        self.next = previous_node
+        if next_node is not None:
+            next_node.reverse_recursively(self)
+
+
+def create_test_linked_list():
+    test_head = ListNode(4)
+    node1 = ListNode(3)
+    test_head.next = node1
+    node2 = ListNode(2)
+    node1.next = node2
+    node3 = ListNode(1)
+    node2.next = node3
+    test_tail = ListNode(0)
+    node3.next = test_tail
+    return test_head, test_tail
+
+
+def main():
+    test_head1, test_tail1 = create_test_linked_list()
+    print("Initial list: ")
+    test_head1.print_list()
+    # 4 3 2 1 0
+    test_head1.reverse_iteratively()
+    print("List after reversal: ")
+    test_tail1.print_list()
+    # 0 1 2 3 4
+
+    test_head2, test_tail2 = create_test_linked_list()
+    print("Initial list: ")
+    test_head2.print_list()
+    # 4 3 2 1 0
+    test_head2.reverse_recursively()
+    print("List after reversal: ")
+    test_tail2.print_list()
+    # 0 1 2 3 4
+
+
+main()
