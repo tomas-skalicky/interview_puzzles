6faffd448a6cbf50192a0bd393728bf144a0428e
Tomas Skalicky <skalicky.tomas@gmail.com>
2020-07-01 19:46:12 +0200

    validate_binary_search_tree.py added

---
+++ src/main/python/com/skalicky/python/interviewpuzzles/validate_binary_search_tree.py
@@ -0,0 +1,91 @@
+# Task:
+#
+# You are given the root of a binary search tree. Return true if it is a valid binary search tree, and false otherwise.
+# Recall that a binary search tree has the property that all values in the left subtree are less than or equal
+# to the root, and all values in the right subtree are greater than or equal to the root.
+#
+# Here's a starting point:
+#
+# class TreeNode:
+#   def __init__(self, key):
+#     self.left = None
+#     self.right = None
+#     self.key = key
+#
+# def is_bst(root):
+#   # Fill this in.
+#
+# a = TreeNode(5)
+# a.left = TreeNode(3)
+# a.right = TreeNode(7)
+# a.left.left = TreeNode(1)
+# a.left.right = TreeNode(4)
+# a.right.left = TreeNode(6)
+# print is_bst(a)
+#
+# #    5
+# #   / \
+# #  3   7
+# # / \ /
+# #1  4 6
+from collections import deque
+from typing import Deque, Tuple
+
+
+class TreeNode:
+    def __init__(self, key):
+        self.left: TreeNode = None
+        self.right: TreeNode = None
+        self.key: int = key
+
+
+def is_bst(root: TreeNode):
+    nodes_to_process: Deque[Tuple[TreeNode, int, int]] = deque()
+    nodes_to_process.append((root, None, None))
+    while len(nodes_to_process) > 0:
+        current_node, min_allowed, max_allowed = nodes_to_process.popleft()
+        if current_node.left is not None:
+            if current_node.left.key >= current_node.key:
+                return False
+            elif min_allowed is not None and current_node.left.key < min_allowed:
+                return False
+            else:
+                nodes_to_process.append((current_node.left, min_allowed, current_node.key - 1))
+        if current_node.right is not None:
+            if current_node.right.key <= current_node.key:
+                return False
+            elif max_allowed is not None and current_node.right.key > max_allowed:
+                return False
+            else:
+                nodes_to_process.append((current_node.right, current_node.key + 1, max_allowed))
+    return True
+
+
+a = TreeNode(5)
+a.left = TreeNode(3)
+a.right = TreeNode(7)
+a.left.left = TreeNode(1)
+a.left.right = TreeNode(4)
+a.right.left = TreeNode(6)
+print(is_bst(a))
+# True
+#
+#     5
+#    / \
+#   3   7
+#  / \ /
+# 1  4 6
+
+a = TreeNode(5)
+a.left = TreeNode(3)
+a.right = TreeNode(7)
+a.left.left = TreeNode(1)
+a.left.right = TreeNode(6)
+print(is_bst(a))
+# False
+#
+#     5
+#    / \
+#   3   7
+#  / \
+# 1  6
