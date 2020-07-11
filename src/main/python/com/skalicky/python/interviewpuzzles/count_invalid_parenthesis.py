7235161f8d38d71f99831f7fe00164cb84ea7398
Tomas Skalicky <skalicky.tomas@gmail.com>
2020-07-11 18:23:23 +0200

    count_invalid_parenthesis.py added

---
+++ src/main/python/com/skalicky/python/interviewpuzzles/count_invalid_parenthesis.py
@@ -0,0 +1,45 @@
+# Task:
+#
+# You are given a string of parenthesis. Return the minimum number of parenthesis that would need to be removed in order
+# to make the string valid. "Valid" means that each open parenthesis has a matching closed parenthesis.
+#
+# Example:
+#
+# "()())()"
+#
+# The following input should return 1.
+#
+# ")("
+#
+# Here's a start:
+#
+# def count_invalid_parenthesis(string):
+#   # Fill this in.
+#
+# print count_invalid_parenthesis("()())()")
+# # 1
+
+
+def count_invalid_parenthesis(string: str):
+    invalid_parenthesis_count: int = 0
+    open_parenthesis_count: int = 0
+    for c in list(string):
+        if c == '(':
+            open_parenthesis_count += 1
+        else:
+            if open_parenthesis_count > 0:
+                open_parenthesis_count -= 1
+            else:
+                invalid_parenthesis_count += 1
+    invalid_parenthesis_count += open_parenthesis_count
+    return invalid_parenthesis_count
+
+
+print(count_invalid_parenthesis(""))
+# 0
+print(count_invalid_parenthesis("()())()"))
+# 1
+print(count_invalid_parenthesis("()(())()"))
+# 0
+print(count_invalid_parenthesis("()()))(()"))
+# 3
