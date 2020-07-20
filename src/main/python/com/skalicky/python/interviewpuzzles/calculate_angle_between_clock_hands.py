43954ad0ca59c67faec9d12f37daa672969ba051
Tomas Skalicky <skalicky.tomas@gmail.com>
2020-07-20 22:46:40 +0200

    calculate_angle_between_clock_hands.py fixed: angle_between_h_and_hour_hand considered

--- src/main/python/com/skalicky/python/interviewpuzzles/calculate_angle_between_clock_hands.py
+++ src/main/python/com/skalicky/python/interviewpuzzles/calculate_angle_between_clock_hands.py
@@ -13,15 +13,20 @@ from math import floor
 
 
 def calc_angle(h: int, m: int):
-    hour_angle_from_12: int = h % 12 * floor(360 / 12)
-    minute_angle_from_12: int = m % 60 * floor(360 / 60)
-    return abs(hour_angle_from_12 - minute_angle_from_12)
+    minutes_per_clock: int = 60
+    hours_per_clock: int = 12
+    angle_between_60_and_m: int = m * floor(360 / minutes_per_clock)
+    angle_between_12_and_h: int = h % hours_per_clock * floor(360 / hours_per_clock)
+    angle_between_h_and_hour_hand: int = floor(angle_between_60_and_m / hours_per_clock)
+    return abs(angle_between_12_and_h + angle_between_h_and_hour_hand - angle_between_60_and_m)
 
 
 print(calc_angle(3, 30))
-# 90
+# 75
+print(calc_angle(15, 30))
+# 75
 print(calc_angle(12, 30))
-# 180
+# 165
 print(calc_angle(1, 0))
 # 30
 print(calc_angle(12, 0))
