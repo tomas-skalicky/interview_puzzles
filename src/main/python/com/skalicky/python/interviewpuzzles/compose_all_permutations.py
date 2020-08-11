86c1bfdd5f11e600aa3d95f5ab3ec38909a5e834
Tomas Skalicky <skalicky.tomas@gmail.com>
2020-08-11 18:17:50 +0200

    compose_all_permutations.py added

---
+++ src/main/python/com/skalicky/python/interviewpuzzles/compose_all_permutations.py
@@ -0,0 +1,45 @@
+# Task:
+#
+# You are given an array of integers. Return all the permutations of this array.
+#
+# def permute(nums):
+#   # Fill this in.
+#
+# print permute([1, 2, 3])
+# # [[1, 2, 3], [2, 1, 3], [2, 3, 1], [1, 3, 2], [3, 1, 2], [3, 2, 1]]
+from typing import List, Set
+
+
+def permute_recursively(num: int, remaining_nums: List[int], current_permutation: List[int], occupied_indices: Set[int],
+                        collected_permutations: List[List[int]]):
+    for i in range(0, len(current_permutation)):
+        if not occupied_indices.__contains__(i):
+            current_permutation[i] = num
+            occupied_indices.add(i)
+            if len(remaining_nums) == 0:
+                collected_permutations.append(current_permutation.copy())
+            else:
+                permute_recursively(remaining_nums[0], remaining_nums[1:], current_permutation, occupied_indices,
+                                    collected_permutations)
+            current_permutation[i] = None
+            occupied_indices.remove(i)
+
+
+# Time and space complexity of this method is O(n!)
+def permute(nums: List[int]) -> List[List[int]]:
+    num_count: int = len(nums)
+    if num_count == 0:
+        return []
+    else:
+        collected_permutations: List[List[int]] = list()
+        current_permutation: List[int] = [None] * num_count
+        permute_recursively(nums[0], nums[1:], current_permutation, set(), collected_permutations)
+        return collected_permutations
+
+
+print(permute([]))
+# [[]]
+print(permute([1]))
+# [[1]]
+print(permute([1, 2, 3]))
+# [[1, 2, 3], [2, 1, 3], [2, 3, 1], [1, 3, 2], [3, 1, 2], [3, 2, 1]]
