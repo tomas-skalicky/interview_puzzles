5c3dcc06933ff4ce3200efe8675a9fe0a8db0e7e
Tomas Skalicky <skalicky.tomas@gmail.com>
2020-08-13 18:25:34 +0200

    find_continuous_subarray_with_target_sum.py added

---
+++ src/main/python/com/skalicky/python/interviewpuzzles/find_continuous_subarray_with_target_sum.py
@@ -0,0 +1,36 @@
+# Task:
+#
+# You are given an array of integers, and an integer K. Return the subarray which sums to K. You can assume that
+# a solution will always exist.
+#
+# def find_continuous_k(list, k):
+#   # Fill this in.
+#
+# print find_continuous_k([1, 3, 2, 5, 7, 2], 14)
+# # [2, 5, 7]
+#
+# Can you do this in linear time?
+from typing import List
+
+
+def find_continuous_k(input_list: List[int], k: int) -> List[int]:
+    current_start_index_included: int = 0
+    current_end_index_included: int = 0
+    current_sum: int = input_list[current_start_index_included]
+    input_size: int = len(input_list)
+    while current_sum != k and current_end_index_included + 1 < input_size:
+        current_end_index_included += 1
+        current_sum += input_list[current_end_index_included]
+        while current_sum > k:
+            current_sum -= input_list[current_start_index_included]
+            current_start_index_included += 1
+    if current_sum == k:
+        return input_list[current_start_index_included:current_end_index_included + 1]
+    else:
+        return None
+
+
+print(find_continuous_k([1, 3, 2, 5, 7, 2], 14))
+# [2, 5, 7]
+print(find_continuous_k([1, 3, 2, 5, 6, 2], 14))
+# None
