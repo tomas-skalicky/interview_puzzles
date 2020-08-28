1161e104ab4327906d1c3c3bd30d30c071864bb7
Tomas Skalicky <skalicky.tomas@gmail.com>
2020-08-28 18:41:39 +0200

    find_longest_run_of_ones_in_binary_representation.py added

---
+++ src/main/python/com/skalicky/python/interviewpuzzles/find_longest_run_of_ones_in_binary_representation.py
@@ -0,0 +1,31 @@
+# Task:
+#
+# Return the longest run of 1s for a given integer n's binary representation.
+#
+# Example:
+# Input: 242
+# Output: 4
+# 242 in binary is 0b11110010, so the longest run of 1 is 4.
+#
+# def longest_run(n):
+#   # Fill this in.
+#
+# print longest_run(242)
+# # 4
+
+
+def longest_run(n: int) -> int:
+    binary_representation: str = bin(n).split('b')[1]
+    max_count: int = 0
+    current_count: int = 0
+    for current_digit in list(binary_representation):
+        if current_digit == '1':
+            current_count += 1
+            max_count = max(max_count, current_count)
+        else:
+            current_count = 0
+    return max_count
+
+
+print(longest_run(242))
+# 4
