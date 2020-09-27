d0f81129ed3f6c6e84ebef36a820486c75837467
Tomas Skalicky <skalicky.tomas@gmail.com>
2020-09-27 19:21:36 +0200

    find_palindrome.py added

---
+++ src/main/python/com/skalicky/python/interviewpuzzles/find_palindrome.py
@@ -0,0 +1,50 @@
+# Task:
+#
+# Given a string, determine if there is a way to arrange the string such that the string is a palindrome. If such
+# arrangement exists, return a palindrome (There could be many arrangements). Otherwise return None.
+#
+# Here's some starter code:
+#
+# def find_palindrome(s):
+#   # Fill this in.
+#
+# print(find_palindrome('momom'))
+# # momom
+from collections import deque
+from math import floor
+from typing import List, Dict, Deque
+
+
+def find_palindrome(s: str) -> str:
+    characters: List[str] = list(s)
+    occurrence_count_by_characters: Dict[str, int] = dict()
+    for current_char in characters:
+        occurrence_count_by_characters[current_char] = occurrence_count_by_characters[
+                                                           current_char] + 1 if occurrence_count_by_characters.__contains__(
+            current_char) else 1
+
+    char_with_odd_occurrences: str = None
+
+    for current_char in occurrence_count_by_characters.keys():
+        if occurrence_count_by_characters[current_char] % 2 == 1:
+            if char_with_odd_occurrences is None:
+                char_with_odd_occurrences = current_char
+            else:
+                return None
+
+    result_string_deque: Deque[str] = deque(char_with_odd_occurrences)
+    for current_char in occurrence_count_by_characters.keys():
+        for i in range(0, floor(occurrence_count_by_characters[current_char] / 2)):
+            result_string_deque.appendleft(current_char)
+            result_string_deque.append(current_char)
+    return ''.join(result_string_deque)
+
+
+print(find_palindrome('momom'))
+# ommmo
+print(find_palindrome('mommo'))
+# ommmo
+print(find_palindrome('mmrrmommo'))
+# ormmmmmro
+print(find_palindrome('moommo'))
+# None
