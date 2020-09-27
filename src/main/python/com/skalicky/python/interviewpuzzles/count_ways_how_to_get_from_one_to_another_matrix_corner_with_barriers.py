883ad874702d42045c3680ce91b74d35b448c644
Tomas Skalicky <skalicky.tomas@gmail.com>
2020-09-27 07:17:22 +0200

    a more efficient algorithm added to count_ways_how_to_get_from_one_to_another_matrix_corner_with_barriers.py

--- src/main/python/com/skalicky/python/interviewpuzzles/count_ways_how_to_get_from_one_to_another_matrix_corner_with_barriers.py
+++ src/main/python/com/skalicky/python/interviewpuzzles/count_ways_how_to_get_from_one_to_another_matrix_corner_with_barriers.py
@@ -24,10 +24,13 @@
 #                           [0, 0, 1],
 #                           [0, 0, 0]]))
 # # 2
-from typing import List, Tuple
+from collections import deque
+from typing import List, Tuple, Deque, Set
 
 
-def paths_through_maze(maze: List[List[int]]) -> int:
+# Time and spacial complexity ... O(row_count! * column_count!). See
+# count_ways_how_to_get_from_one_to_another_matrix_corner.py
+def paths_through_maze_traversing_without_memory(maze: List[List[int]]) -> int:
     row_count: int = len(maze)
     if row_count == 0:
         return 0
@@ -39,6 +42,7 @@ def paths_through_maze(maze: List[List[int]]) -> int:
             max_row_index: int = row_count - 1
             max_column_index: int = column_count - 1
             counter: int = 0
+
             last_positions: List[Tuple[int, int]] = [(0, 0)]
             while len(last_positions) > 0:
                 current_row_index, current_column_index = last_positions.pop()
@@ -53,19 +57,81 @@ def paths_through_maze(maze: List[List[int]]) -> int:
             return counter
 
 
-print(paths_through_maze([]))
+# Time and spacial complexity ... O(n * m)
+def paths_through_maze_dynamic_programming(maze: List[List[int]]) -> int:
+    row_count: int = len(maze)
+    if row_count == 0:
+        return 0
+    else:
+        column_count: int = len(maze[0])
+        if column_count == 0:
+            return 0
+        else:
+            max_row_index: int = row_count - 1
+            max_column_index: int = column_count - 1
+            maze_with_remembered_combinations: List[List[int]] = [[0 for j in range(0, column_count)] for i in
+                                                                  range(0, row_count)]
+
+            cells_to_process: Deque[Tuple[int, int]] = deque()
+            processed_and_to_be_processed_sells: Set[Tuple[int, int]] = set()
+            top_left_corner: Tuple[int, int] = (0, 0)
+            cells_to_process.append(top_left_corner)
+            processed_and_to_be_processed_sells.add(top_left_corner)
+            while len(cells_to_process) > 0:
+                current_row_index, current_column_index = cells_to_process.popleft()
+
+                if current_row_index == 0 and current_column_index == 0:
+                    maze_with_remembered_combinations[current_row_index][current_column_index] = 1
+                else:
+                    from_top: int = maze_with_remembered_combinations[current_row_index - 1][
+                        current_column_index] if current_row_index > 0 else 0
+                    from_left: int = maze_with_remembered_combinations[current_row_index][
+                        current_column_index - 1] if current_column_index > 0 else 0
+                    maze_with_remembered_combinations[current_row_index][current_column_index] = from_top + from_left
+
+                if current_row_index < max_row_index and not processed_and_to_be_processed_sells.__contains__(
+                        (current_row_index + 1, current_column_index)) and \
+                        maze[current_row_index + 1][current_column_index] == 0:
+                    new_cell_to_process: Tuple[int, int] = (current_row_index + 1, current_column_index)
+                    cells_to_process.append(new_cell_to_process)
+                    processed_and_to_be_processed_sells.add(new_cell_to_process)
+
+                if current_column_index < max_column_index and not processed_and_to_be_processed_sells.__contains__(
+                        (current_row_index, current_column_index + 1)) and maze[current_row_index][
+                    current_column_index + 1] == 0:
+                    new_cell_to_process: Tuple[int, int] = (current_row_index, current_column_index + 1)
+                    cells_to_process.append(new_cell_to_process)
+                    processed_and_to_be_processed_sells.add(new_cell_to_process)
+
+            return maze_with_remembered_combinations[max_row_index][max_column_index]
+
+
+def paths_through_maze(maze: List[List[int]]) -> None:
+    print(paths_through_maze_traversing_without_memory(maze))
+    print(paths_through_maze_dynamic_programming(maze))
+
+
+paths_through_maze([])
 # 0
-print(paths_through_maze([[]]))
 # 0
-print(paths_through_maze([[0, 1, 0],
-                          [0, 0, 1],
-                          [0, 0, 0]]))
+paths_through_maze([[]])
+# 0
+# 0
+paths_through_maze([[0]])
+# 1
+# 1
+paths_through_maze([[0, 1, 0],
+                    [0, 0, 1],
+                    [0, 0, 0]])
 # 2
-print(paths_through_maze([[0, 0, 0],
-                          [0, 0, 1],
-                          [0, 0, 0]]))
+# 2
+paths_through_maze([[0, 0, 0],
+                    [0, 0, 1],
+                    [0, 0, 0]])
 # 3
-print(paths_through_maze([[0, 0, 0],
-                          [0, 0, 0],
-                          [0, 0, 0]]))
+# 3
+paths_through_maze([[0, 0, 0],
+                    [0, 0, 0],
+                    [0, 0, 0]])
+# 6
 # 6
