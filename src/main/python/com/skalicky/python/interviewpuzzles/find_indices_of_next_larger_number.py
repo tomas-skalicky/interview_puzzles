0286400e70bc6365444d9d174e1b114e7fb1123a
Tomas Skalicky <skalicky.tomas@gmail.com>
2020-11-06 07:34:36 +0100

    find_indices_of_next_larger_number.py: a provided binary search for ascendingly sorted lists replaced with custom adaptation for descendingly sorted lists. Reason: List can more efficiently work with the last element than with the first one.

--- src/main/python/com/skalicky/python/interviewpuzzles/find_indices_of_next_larger_number.py
+++ src/main/python/com/skalicky/python/interviewpuzzles/find_indices_of_next_larger_number.py
@@ -10,33 +10,60 @@
 #
 # # print [2, 2, 3, 4, -1, -1]
 # print(larger_number([3, 2, 5, 6, 9, 8]))
-from bisect import insort_left
+from math import floor
 from typing import List, Dict
 
 
-# Time complexity ... O(n log n) where n is a size of the input list
+def bisect_left_for_descendingly_sorted_list(input_list, searched_element):
+    """Simplified copy of bisect.bisect_left adapted for input list sorted descendingly.
+    """
+
+    lower_bound: int = 0
+    higher_bound: int = len(input_list)
+    while lower_bound < higher_bound:
+        middle_index: int = floor((lower_bound + higher_bound) / 2)
+        if input_list[middle_index] < searched_element:
+            higher_bound = middle_index
+        else:
+            lower_bound = middle_index + 1
+    return lower_bound
+
+
+def insort_left_for_descendingly_sorted_list(input_list, searched_element):
+    """Simplified copy of bisect.insort_left adapted for input list sorted descendingly.
+    """
+
+    insert_index = bisect_left_for_descendingly_sorted_list(input_list, searched_element)
+    input_list.insert(insert_index, searched_element)
+
+
 def find_indices_of_next_larger_number(input_numbers: List[int]) -> List[int]:
+    """Time complexity ... O(n log n) where n is a size of the input list
+    """
+
     result_indices: List[int] = [-1 for i in range(0, len(input_numbers))]
     index_lists_by_searched_closest_larger_numbers: Dict[int, List[int]] = {}
-    searched_closest_larger_numbers: List[int] = []
+    # Ordered descendingly since the insert and remove operation at the end of List has a time complexity of O(1).
+    # The insert and remove operation at the beginning of List has however a time complexity of O(n)
+    sorted_searched_closest_larger_numbers_ordered_desc: List[int] = []
     for i in range(0, len(input_numbers)):
         current_number: int = input_numbers[i]
         # Each element in the list will be processed by the inner for loop at most once.
-        while len(searched_closest_larger_numbers) > 0:
-            searched_number: int = searched_closest_larger_numbers.pop(0)
+        while len(sorted_searched_closest_larger_numbers_ordered_desc) > 0:
+            searched_number: int = sorted_searched_closest_larger_numbers_ordered_desc.pop()
             if searched_number <= current_number:
                 for non_processed_index in index_lists_by_searched_closest_larger_numbers.pop(searched_number):
                     result_indices[non_processed_index] = i
             else:
-                searched_closest_larger_numbers.insert(0, searched_number)
+                sorted_searched_closest_larger_numbers_ordered_desc.append(searched_number)
                 break
 
         searched_closest_larger_number: int = current_number + 1
-        if searched_closest_larger_numbers.__contains__(searched_closest_larger_number):
+        if index_lists_by_searched_closest_larger_numbers.__contains__(searched_closest_larger_number):
             index_lists_by_searched_closest_larger_numbers[searched_closest_larger_number].append(i)
         else:
             # Time complexity ... O(log n)
-            insort_left(searched_closest_larger_numbers, searched_closest_larger_number)
+            insort_left_for_descendingly_sorted_list(sorted_searched_closest_larger_numbers_ordered_desc, searched_closest_larger_number)
             index_lists_by_searched_closest_larger_numbers[searched_closest_larger_number] = [i]
 
     return result_indices
