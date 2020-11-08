c3f1cd230a9e272a6962cd8fe1ebb6c928c5a82e
Tomas Skalicky <skalicky.tomas@gmail.com>
2020-11-08 19:04:55 +0100

    multiply_2_large_numbers.py implemented

--- src/main/python/com/skalicky/python/interviewpuzzles/multiply_2_large_numbers.py
+++ src/main/python/com/skalicky/python/interviewpuzzles/multiply_2_large_numbers.py
@@ -8,7 +8,52 @@
 #
 # def multiply(str1, str2):
 #   # Fill this in.
+from collections import deque
+from typing import List, Deque
 
 
-def multiply_2_large_numbers(number_string1: str, number_string2: str) -> str:
-    return
+def multiply_2_large_numbers(number1_string: str, number2_string: str) -> str:
+    if number1_string == '0' or number2_string == '0':
+        return '0'
+    else:
+        number1_digits_reversed: List[str] = list(number1_string)
+        number1_digits_reversed.reverse()
+        number2_digits_reversed: List[str] = list(number2_string)
+        number2_digits_reversed.reverse()
+        reversed_intermediate_multiplications: Deque[str] = deque()
+        trailing_zeros: str = ''
+        for number1_digit_string in number1_digits_reversed:
+            overflow: int = 0
+            reversed_intermediate_multiplication: str = trailing_zeros
+            for number2_digit_string in number2_digits_reversed:
+                intermediate_result: int = int(number1_digit_string) * int(number2_digit_string) + overflow
+                digit_to_add: int = intermediate_result % 10
+                reversed_intermediate_multiplication += str(digit_to_add)
+                overflow = int((intermediate_result - digit_to_add) / 10)
+            reversed_intermediate_multiplications.append(reversed_intermediate_multiplication)
+            trailing_zeros += '0'
+
+        result: str = ''
+        intermediate_multiplication_count: int = len(reversed_intermediate_multiplications)
+        max_length: int = len(reversed_intermediate_multiplications[intermediate_multiplication_count - 1])
+        overflow: int = 0
+        for i in range(0, max_length):
+
+            remove_first: bool = False
+            first_processed: bool = False
+            intermediate_sum: int = overflow
+            for reversed_intermediate_multiplication in reversed_intermediate_multiplications:
+                if i < len(reversed_intermediate_multiplication):
+                    intermediate_sum += int(reversed_intermediate_multiplication[i])
+                if first_processed:
+                    first_processed = True
+                    if i >= len(reversed_intermediate_multiplication):
+                        remove_first = True
+
+            digit_to_add: int = intermediate_sum % 10
+            result = str(digit_to_add) + result
+            overflow = int((intermediate_sum - digit_to_add) / 10)
+
+            if remove_first:
+                reversed_intermediate_multiplications.pop()
+        return result
--- src/test/python/com/skalicky/python/interviewpuzzles/test_multiply_2_large_numbers.py
+++ src/test/python/com/skalicky/python/interviewpuzzles/test_multiply_2_large_numbers.py
@@ -6,3 +6,9 @@ from src.main.python.com.skalicky.python.interviewpuzzles.multiply_2_large_numbe
 class Test(TestCase):
     def test_multiply_2_large_numbers__when_11_times_13__then_result_is_143(self):
         self.assertEqual('143', multiply_2_large_numbers('11', '13'))
+
+    def test_multiply_2_large_numbers__when_211_times_211__then_result_is_44521(self):
+        self.assertEqual('44521', multiply_2_large_numbers('211', '211'))
+
+    def test_multiply_2_large_numbers__when_211_times_0__then_result_is_0(self):
+        self.assertEqual('0', multiply_2_large_numbers('211', '0'))
