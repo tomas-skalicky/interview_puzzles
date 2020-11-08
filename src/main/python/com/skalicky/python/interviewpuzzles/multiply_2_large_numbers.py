614e4b0ec3b732ffebfc20bc54d7bddf5ef06ccc
Tomas Skalicky <skalicky.tomas@gmail.com>
2020-11-08 18:13:20 +0100

    multiply_2_large_numbers.py with unit tests added, but implemented yet

---
+++ src/main/python/com/skalicky/python/interviewpuzzles/multiply_2_large_numbers.py
@@ -0,0 +1,14 @@
+# Task:
+#
+# Given two strings which represent non-negative integers, multiply the two numbers and return the product as a string
+# as well. You should assume that the numbers may be sufficiently large such that the built-in integer type will not
+# be able to store the input (Python does have BigNum, but assume it does not exist).
+#
+# Here's an example and some starter code.
+#
+# def multiply(str1, str2):
+#   # Fill this in.
+
+
+def multiply_2_large_numbers(number_string1: str, number_string2: str) -> str:
+    return
---
+++ src/test/python/com/skalicky/python/interviewpuzzles/test_multiply_2_large_numbers.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+from src.main.python.com.skalicky.python.interviewpuzzles.multiply_2_large_numbers import multiply_2_large_numbers
+
+
+class Test(TestCase):
+    def test_multiply_2_large_numbers__when_11_times_13__then_result_is_143(self):
+        self.assertEqual('143', multiply_2_large_numbers('11', '13'))
